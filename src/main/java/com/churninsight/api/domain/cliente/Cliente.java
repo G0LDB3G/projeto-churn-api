bd9bb4453a64a22f571a85a0aaf1af9eb3fc9eaf
Rob <robertorodriguesre@gmail.com>
2025-12-30 09:50:20 -0300

    adicionando banco de dados

--- src/main/java/com/churninsight/api/domain/cliente/Cliente.java
+++ src/main/java/com/churninsight/api/domain/cliente/Cliente.java
@@ -1,4 +1,23 @@
 package com.churninsight.api.domain.cliente;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Entity
+@Getter
+@EqualsAndHashCode(of = "id")
+
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    private String nome;
+    private String token;
+
 }
--- src/main/java/com/churninsight/api/domain/cliente/repository/ClienteRepository.java
+++ src/main/java/com/churninsight/api/domain/cliente/repository/ClienteRepository.java
@@ -1,4 +1,13 @@
 package com.churninsight.api.domain.cliente.repository;
 
-public interface ClienteRepository {
+import com.churninsight.api.domain.cliente.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    Optional<Cliente> findByToken(String token);
 }
